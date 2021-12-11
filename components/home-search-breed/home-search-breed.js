507e9b3d495e0c1aebb5f79f64c7dd6f80ab0ebd
mecamon <60249045+mecamon@users.noreply.github.com>
2021-12-11 13:03:19 -0400

    ADDED DESCRIPTION TO THE PAGE

--- components/home-search-breed/home-search-breed.js
+++ components/home-search-breed/home-search-breed.js
@@ -37,7 +37,7 @@ export default function HomeSearchBreed({onFetchBreeds, suggestedBreeds, hideSug
         <ul data-testid="breed-suggested-group" 
           className="absolute bg-white rounded-3xl px-3 py-5 mt-4 w-full max-h-52 overflow-y-scroll custom-scrollbar">{ 
           suggestedBreeds.map(breed => 
-          <li key={breed.id} data-testid="breed-suggested" className=" hover:bg-secondary rounded-xl px-3 hover:bg-opacity-10 py-3">
+          <li key={breed.id} data-testid="breed-suggested" className=" hover:bg-secondary cursor-pointer rounded-xl px-3 hover:bg-opacity-10 py-3">
             <Link href={`/breed/${breed.id}`}>
               <span className="z-30 opacity-1">{breed.catName}</span>
             </Link>
