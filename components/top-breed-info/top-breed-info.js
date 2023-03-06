9e3af5e5024468d0ae3e773dc1cda767180ac4d8
mecamon <arq.camejia@gmail.com>
2023-03-06 11:44:53 -0400

    images object-cover in thumbnails

--- components/top-breed-info/top-breed-info.js
+++ components/top-breed-info/top-breed-info.js
@@ -1,20 +1,33 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function TopBreedInfo({breed, index}) {
-  return(
+export default function TopBreedInfo({ breed, index }) {
+  return (
     <div className="flex flex-col sm:flex-row">
       <div>
         <div className="h-44 w-44 rounded-3xl overflow-hidden">
-          <img src={breed.image} className=" h-44 w-auto rounded-3xl" alt="breed photo" data-testid="breed-image" />
+          <img
+            src={breed.image}
+            className=" h-44 object-cover w-auto rounded-3xl"
+            alt="breed photo"
+            data-testid="breed-image"
+          />
         </div>
       </div>
       <div className="flex flex-col sm:ml-11">
         <Link href={`/breed/${breed.originalId}`}>
           <a>
-            <h3 data-testid="breed-name" className="font-semibold hover:underline text-lg md:text-3xl mt-3 md:mt-0 mb-2 md:mb-5">{`${index + 1}. ${breed.name}`}</h3>
+            <h3
+              data-testid="breed-name"
+              className="font-semibold hover:underline text-lg md:text-3xl mt-3 md:mt-0 mb-2 md:mb-5"
+            >{`${index + 1}. ${breed.name}`}</h3>
           </a>
         </Link>
-        <p data-testid="breed-description" className=" text-sm md:text-lg font-medium">{breed.description}</p>
+        <p
+          data-testid="breed-description"
+          className=" text-sm md:text-lg font-medium"
+        >
+          {breed.description}
+        </p>
       </div>
     </div>
   );
--- data/gql-client.js
+++ data/gql-client.js
@@ -1,10 +1,13 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const uri = process.env.NODE_ENV === 'development' ? 'http://localhost:4000/' : 'https://catapi.meca.dev/';
+const uri =
+  process.env.NODE_ENV === "development"
+    ? "https://catapi.meca.dev/"
+    : "https://catapi.meca.dev/";
 
 const client = new ApolloClient({
   uri,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 export default client;
